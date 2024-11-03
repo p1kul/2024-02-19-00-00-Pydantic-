35adef706498e6b60dc92b8e28c526078198896b
p1kul <funikova@me.com>
2024-11-03 14:32:03 +0300

    Add files via upload

--- ls.py
+++ ls.py
@@ -14,34 +14,32 @@ async def all_inf():
     return users
 
 @app.post("/user/{username}/{age}")
-async def create_message(user: User):
+async def post_user(username: str, age: int, user: User):
     if not users:
         user_id = 1
     else:
        user_id = users[-1].id + 1
     user.id = user_id
+    user.age = age
+    user.username = username
     users.append(user)
     return user
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def refresh(user_id: int, username: str, age: int, user: str = Body()):
+async def refresh_user(user_id: int, username: str, age: int, user: str = Body()):
     try:
-        users[user_id].username = username
-        users[user_id].age = age
+        refresh = users[user_id - 1]
+        refresh.username = username
+        refresh.age = age
+        return refresh
     except IndexError:
-        raise HTTPException(status_code=404, detail="Message not found")
+        raise HTTPException(status_code=404, detail="User not found")
 
 @app.delete("/user/{user_id}")
-async def delete(user_id: int ) -> str:
-    try:
-        user = users.pop(user_id - 1)
-    except IndexError:
-        raise HTTPException(status_code=404, detail="Message not found")
-
-
-
-
-
-
+async def delete_user(user_id: int):
+     if user_id >= len(users) or user_id < 0:
+         raise HTTPException(status_code=404, detail="User not found")
+     del_user = users.pop(user_id - 1)
+     return del_user
 
 
