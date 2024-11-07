4a75d6d959da989c9e2cfb78b29ac98623dde54c
p1kul <funikova@me.com>
2024-11-07 14:36:49 +0300

    Add files via upload

--- ls.py
+++ ls.py
@@ -14,31 +14,25 @@ async def all_inf():
     return users
 
 @app.post("/user/{username}/{age}")
-async def post_user(username: str, age: int, user: User):
-    if not users:
-        user_id = 1
-    else:
-       user_id = users[-1].id + 1
-    user.id = user_id
-    user.age = age
-    user.username = username
+async def post_user(user: User):
+    user.id = len(users)
     users.append(user)
     return user
 
 @app.put("/user/{user_id}/{username}/{age}")
 async def refresh_user(user_id: int, username: str, age: int, user: str = Body()):
     try:
-        refresh = users[user_id - 1]
+        refresh = users[user_id]
         refresh.username = username
         refresh.age = age
         return refresh
     except IndexError:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="User was not found")
 
 @app.delete("/user/{user_id}")
 async def delete_user(user_id: int):
      if user_id >= len(users) or user_id < 0:
-         raise HTTPException(status_code=404, detail="User not found")
+         raise HTTPException(status_code=404, detail="User was not found")
      del_user = users.pop(user_id - 1)
      return del_user
 
